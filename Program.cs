59d0c867a4c5f2ff039c5d847e2d3c37469b262b
Mbktec <moubarak.yam@gmail.com>
2023-05-24 15:01:10 +0100

    console app

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+const int nbreM = 7;
+Console.WriteLine("Veuillez saisir votre prenom");
+string nomJoueur = Console.ReadLine();
+Console.WriteLine("Veuillez saisir un nombre entre 1 et 10");
+int nbSaisi = Convert.ToInt32(Console.ReadLine());
+if( nbreM == nbSaisi) {
+    System.Console.WriteLine($"Bravo! {nomJoueur};vous avez gagner");
+}else{
+    if(nbreM<nbSaisi){
+        System.Console.WriteLine("le nombre est plus grand");
+    }else {
+        System.Console.WriteLine("le nombre est plus petit");
+    }
+}
